24ed45da32bc525bbab8b44d5c1ae573fcf68980
otoshi kami <kirito2932000@gmail.com>
2025-05-07 19:40:39 +0700

    Sửa giao diện user 2

--- lib/screens/BaoCao/online_screen.dart
+++ lib/screens/BaoCao/online_screen.dart
@@ -286,7 +286,7 @@ class _OnlineScreenState extends State<OnlineScreen> {
     } else if (exerciseNameLower.contains('gập bụng') ||
         exerciseNameLower.contains('cycle') ||
         exerciseNameLower.contains('bike')) {
-      iconData = Icons.directions_bike;
+      iconData = Icons.self_improvement;
       iconColor = Colors.green;
     } else if (exerciseNameLower.contains('hít đất') ||
         exerciseNameLower.contains('push up') ||
